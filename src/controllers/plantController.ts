b8a0611a862512401002b6d0a2957fb9a58b6c36
Guadalupe <38482942+guadalavila@users.noreply.github.com>
2021-06-13 18:28:47 -0300

    change response api Plant

--- src/controllers/plantController.ts
+++ src/controllers/plantController.ts
@@ -4,7 +4,7 @@ import { Plant } from "../models";
 export const index = async (req: Request, res: Response) => {
   try {
     const products = await Plant.find();
-    res.status(200).json({ data: products });
+    res.status(200).json(products);
   } catch (error) {
     res.status(500).json({ message: "Error in getting products" });
   }
