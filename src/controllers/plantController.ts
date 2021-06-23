723700b88f8b236c794d7dd0d85ab35d4f3048a5
Guadalupe <guadalupe.ines.alavila@everis.com>
2021-06-23 01:42:42 -0300

    split api plants

--- src/controllers/plantController.ts
+++ src/controllers/plantController.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express";
 import { Plant } from "../models";
+import { Plant as PlantType } from "../types";
 
 export const index = async (req: Request, res: Response) => {
   try {
-    const products = await Plant.find();
-    res.status(200).json(products);
+      const indoor = await Plant.find({})
+        .where('houseplant').equals(true);
+      const outdoor = await Plant.find({})
+        .where('houseplant').equals(false);
+    res.status(200).json({indoor, outdoor});
   } catch (error) {
     res.status(500).json({ message: "Error in getting products" });
   }
---
+++ src/types/plant.ts
@@ -0,0 +1,11 @@
+
+export type Plant = {
+  _id: string;
+  code: string;
+  name: string;
+  description: string;
+  imageUrl: string;
+  price: number;
+  stock: boolean;
+  houseplant: boolean;
+};
